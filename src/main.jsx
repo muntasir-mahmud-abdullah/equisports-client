72925294c6fac420d55ba8b4383ca9fcdd5b7b1c
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-12-07 20:06:59 +0600

    react routing basic set up updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,39 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AddEquipment from "./pages/AddEquipment.jsx";
 import UpdateEquipment from "./pages/UpdateEquipment.jsx";
+import SignIn from "./components/SignIn.jsx";
+import SignUp from "./components/SignUp.jsx";
+import Layout from "./components/Layout.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
-    loader: () => fetch("http://localhost:5000/equipments"),
-  },
-  {
-    path: "addEquipment",
-    element: <AddEquipment></AddEquipment>,
-  },
-  {
-    path: "updateEquipment/:id",
-    element: <UpdateEquipment></UpdateEquipment>,
-    loader: ({ params }) =>
-      fetch(`http://localhost:5000/equipments/${params.id}`),
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <App></App>,
+        loader: () => fetch("http://localhost:5000/equipments"),
+      },
+      {
+        path: "addEquipment",
+        element: <AddEquipment></AddEquipment>,
+      },
+
+      {
+        path: "updateEquipment/:id",
+        element: <UpdateEquipment></UpdateEquipment>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/equipments/${params.id}`),
+      },
+      {
+        path: "signIn",
+        element: <SignIn></SignIn>,
+      },
+      {
+        path: "signUp",
+        element: <SignUp></SignUp>,
+      },
+    ],
   },
 ]);
 
